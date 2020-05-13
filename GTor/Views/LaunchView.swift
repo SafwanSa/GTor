4082808e8740d2307d253d4fa9eb5ddbd029255c
Safwan <safwan9f@gmail.com>
2020-05-14 01:31:34 +0300

    Change the name of the BaseScreen to LaunchViw

--- GTor/Views/LaunchView.swift
+++ GTor/Views/LaunchView.swift
@@ -8,14 +8,14 @@
 
 import SwiftUI
 
-struct BaseScreen: View {
+struct LaunchView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct BaseScreen_Previews: PreviewProvider {
+struct LaunchView_Previews: PreviewProvider {
     static var previews: some View {
-        BaseScreen()
+        LaunchView()
     }
 }
