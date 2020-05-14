4ac27d250895ac5a20161e4b69e0afff19931687
Safwan <safwan9f@gmail.com>
2020-05-14 03:13:29 +0300

    Add the Importance Enum

--- GTor/Models/Goal.swift
+++ GTor/Models/Goal.swift
@@ -8,11 +8,17 @@
 
 import Foundation
 
+enum Importance: Int, Codable {
+    case notImportant = 1
+    case important = 2
+    case veryImportant = 3
+}
+
 struct Goal: Codable, Identifiable {
     let id = UUID()
     var title: String
     var note: String
-    var importance: Int
+    var importance: Importance
     var satisfaction: Double
     var dueDate: Date
     var categories: [Category]
