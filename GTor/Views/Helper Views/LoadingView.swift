1a0c3950400b797cc399570ea3da0adf2eaa102c
Safwan <safwan9f@gmail.com>
2020-05-20 07:16:30 +0300

    Add Lottie view, loading view rearrange some files

---
+++ GTor/Views/Helper Views/LoadingView.swift
@@ -0,0 +1,33 @@
+//
+//  LoadingView.swift
+//  GTor
+//
+//  Created by Safwan Saigh on 20/05/2020.
+//  Copyright © 2020 Safwan Saigh. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    @Binding var isLoading: Bool
+    
+    var body: some View {
+        ZStack {
+            if isLoading {
+                BlurView(style: .systemMaterial)
+                    .edgesIgnoringSafeArea(.all)
+                
+                LottieView(filename: "loading")
+                    .frame(width: 200, height: 200)
+            }
+        }
+        .transition(.opacity)
+        .animation(.linear(duration: 0.2))
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView(isLoading: .constant(true))
+    }
+}
---
+++ GTor/Views/Helper Views/LottieView.swift
@@ -0,0 +1,65 @@
+//
+//  LottieView.swift
+//  GTor
+//
+//  Created by Safwan Saigh on 20/05/2020.
+//  Copyright © 2020 Safwan Saigh. All rights reserved.
+//
+
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    typealias UIViewType = UIView
+    var filename: String
+    
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView(frame: .zero)
+        
+        let animationView = AnimationView()
+        let animation = Animation.named(filename)
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<LottieView>) {
+        
+    }
+}
+
+struct BlurView: UIViewRepresentable {
+
+    let style: UIBlurEffect.Style
+
+    func makeUIView(context: UIViewRepresentableContext<BlurView>) -> UIView {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .clear
+        let blurEffect = UIBlurEffect(style: style)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        view.insertSubview(blurView, at: 0)
+        NSLayoutConstraint.activate([
+            blurView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            blurView.widthAnchor.constraint(equalTo: view.widthAnchor),
+        ])
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView,
+                      context: UIViewRepresentableContext<BlurView>) {
+
+    }
+
+}
