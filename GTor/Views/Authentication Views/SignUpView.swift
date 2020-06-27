b601fda11f98cda324bd8c95e7eb2ede208828d6
Safwan <safwan9f@gmail.com>
2020-06-27 06:12:01 +0300

    Setup loading views

--- GTor/Views/Authentication Views/SignUpView.swift
+++ GTor/Views/Authentication Views/SignUpView.swift
@@ -17,73 +17,83 @@ struct SignUpView: View {
     @State var password = ""
     var isNewUser = false
     @State var alertMessage = ""
+    @State var isLoading = false
     
     var body: some View {
-        Group {
-            if self.userService.authState == .udefined || self.userService.authState == .signOut {
-                VStack {
-                    Text(isNewUser ? "Sign up" : "Sign in")
-                        .font(.title)
-                    
-                    VStack(spacing: 5.0) {
-                        TextField("Email", text: $email)
-                            .autocapitalization(.none)
-                            .keyboardType(.emailAddress)
-                            .padding()
-                            .background(Color("Level 1"))
-                            .clipShape(RoundedRectangle(cornerRadius: 5))
-                            .shadow()
+        ZStack {
+            Group {
+                if self.userService.authState == .udefined || self.userService.authState == .signOut {
+                    VStack {
+                        Text(isNewUser ? "Sign up" : "Sign in")
+                            .font(.title)
                         
-                        TextField("Password (At least 6 numbers)", text: $password)
-                            .autocapitalization(.none)
-                            .padding()
-                            .background(Color("Level 1"))
-                            .clipShape(RoundedRectangle(cornerRadius: 5))
-                            .shadow()
+                        VStack(spacing: 5.0) {
+                            TextField("Email", text: $email)
+                                .autocapitalization(.none)
+                                .keyboardType(.emailAddress)
+                                .padding()
+                                .background(Color("Level 1"))
+                                .clipShape(RoundedRectangle(cornerRadius: 5))
+                                .shadow()
+                            
+                            TextField("Password (At least 6 numbers)", text: $password)
+                                .autocapitalization(.none)
+                                .padding()
+                                .background(Color("Level 1"))
+                                .clipShape(RoundedRectangle(cornerRadius: 5))
+                                .shadow()
+                            
+                            Text(alertMessage)
+                                .multilineTextAlignment(.center)
+                        }
+                        .padding(20)
                         
-                        Text(alertMessage)
-                            .multilineTextAlignment(.center)
+                        Button(action: isNewUser ? signup : signin) {
+                            Text("Go")
+                                .font(.system(size: 25))
+                                .frame(maxWidth: .infinity)
+                                .padding(.vertical, 10)
+                                .background(Color("Level 4"))
+                                .foregroundColor(Color("Level 0"))
+                                .clipShape(RoundedRectangle(cornerRadius: 5))
+                                .shadow()
+                        }
+                        .padding(20)
                     }
-                    .padding(20)
-                    
-                    Button(action: isNewUser ? signup : signin) {
-                        Text("Go")
-                            .font(.system(size: 25))
-                            .frame(maxWidth: .infinity)
-                            .padding(.vertical, 10)
-                            .background(Color("Level 4"))
-                            .foregroundColor(Color("Level 0"))
-                            .clipShape(RoundedRectangle(cornerRadius: 5))
-                            .shadow()
-                    }
-                    .padding(20)
+                }else {
+                    TabBar()
                 }
-            }else {
-                TabBar()
             }
+            LoadingView(isLoading: self.$isLoading)
         }
         
     }
     
     
     func signup() {
+        isLoading = true
         self.authService.createUser(name: "Tester.", email: email, password: password) { (result) in
             switch result {
             case .failure(let error):
                 self.alertMessage = error.localizedDescription
+                self.isLoading = false
             case .success(()):
                 self.userService.configureAuthStateDidChangeListner()
+                self.isLoading = false
             }
         }
     }
     
     func signin() {
+        isLoading = true
         self.authService.signInUser(email: email, password: password) { (result) in
             switch result {
             case .failure(let error):
                 self.alertMessage = error.localizedDescription
+                self.isLoading = false
             case .success(()):
                 self.userService.configureAuthStateDidChangeListner()
+                self.isLoading = false
             }
         }
     }
