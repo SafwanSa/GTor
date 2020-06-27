a164e67e7131f9e3e18872617ce8730345a5b9fd
Safwan <safwan9f@gmail.com>
2020-06-27 06:17:20 +0300

    Disable darkmode

--- GTor/SceneDelegate.swift
+++ GTor/SceneDelegate.swift
@@ -21,7 +21,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Create the SwiftUI view that provides the window contents.
         let contentView = LoginView()
-
+        if #available(iOS 13.0, *) {
+            window?.overrideUserInterfaceStyle = .light
+        }
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
