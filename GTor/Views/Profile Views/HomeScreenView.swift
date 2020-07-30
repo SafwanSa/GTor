3b8f10eab2ad8d83e511057d985970e0302734b0
Safwan <safwan9f@gmail.com>
2020-07-30 18:28:40 +0300

    Implement settings rows

--- GTor/Views/Profile Views/HomeScreenView.swift
+++ GTor/Views/Profile Views/HomeScreenView.swift
@@ -8,30 +8,53 @@
 
 import SwiftUI
 
+struct Setting: Identifiable {
+    var id = UUID()
+    var text: String
+    var isHavingDestination: Bool
+    var destination: AnyView?
+}
+
+let typicalSettings: [Setting] = [
+    .init(text: "Rate GTor", isHavingDestination: false),
+    .init(text: "Share GTor", isHavingDestination: false),
+    .init(text: "About GTor", isHavingDestination: true)
+]
+let appSettings: [Setting] = [
+    .init(text: "Tags Settings", isHavingDestination: true)
+]
+let actionSettings: [Setting] = [
+    .init(text: "Logout", isHavingDestination: false),
+    .init(text: "Delete Account", isHavingDestination: false)
+]
+
 struct HomeScreenView: View {
     @ObservedObject var userService = UserService.shared
     @ObservedObject var taskService = TaskService.shared
     @State var selectedDate: Date = Date()
-    @State var isShowingDashboard = true
+    @State var isShowingDashboard = false
     
     var body: some View {
-        VStack(spacing: 27.0) {
-            HeaderHomeView(isShowingDashboard: $isShowingDashboard)
+        ScrollView(showsIndicators: false) {
+            VStack(spacing: 27.0) {
+                HeaderHomeView(isShowingDashboard: $isShowingDashboard)
 
-            HStack {
-                Group {
-                    if isShowingDashboard {
-                        CurrentTasksView()
-                    }else {
-                        SettingsView()
+                HStack {
+                    Group {
+                        if isShowingDashboard {
+                            CurrentTasksView()
+                        }else {
+                            SettingsView()
+                        }
                     }
                 }
-            }
-            .transition(.slide)
-            .animation(.spring())
+                .transition(.slide)
+                .animation(.spring())
 
-            Spacer()
+                Spacer()
+            }
         }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
@@ -76,6 +99,7 @@ struct HeaderHomeView: View {
             .foregroundColor(Color("Primary"))
             .padding(.horizontal, 45)
             .padding(.vertical, 22)
+            .padding(.top, 35)
             .background(Color(#colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.9960784314, alpha: 1)))
             .clipShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
             .shadow(color: Color.black.opacity(0.12), radius: 20, x: 0, y: 7)
@@ -89,12 +113,59 @@ struct CurrentTasksView: View {
     
     var body: some View {
         VStack(spacing: 27.0) {
-            DaysCardView(selectedDate: $selectedDate)
+//            DaysCardView(selectedDate: $selectedDate)
             
             ForEach(taskService.tasks.filter { !$0.isSatisfied && ($0.dueDate == nil ? true : $0.dueDate!.fullDate == self.selectedDate.fullDate) }) { task in
                 NewTaskCardView(task: task, selectedTask: .constant(Task.dummy))
                     .padding(.horizontal)
             }
         }
+    }
+}
+
+struct SettingsRowButtonView: View {
+    var text: String
+    var isHavingDestination: Bool
+    
+    var body: some View {
+        VStack(spacing: 27.0) {
+                HStack {
+                    Text(text)
+                    Spacer()
+                    Image(systemName: "chevron.right").opacity(isHavingDestination ? 1 : 0)
+                }
+                .padding(22)
+                .background(Color(text == "Delete Account" ? "Level 3" : "Level 0"))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .elevation()
+                .foregroundColor(Color("Primary"))
+        }
+        .foregroundColor(Color("Primary"))
+    }
+}
+
+struct SettingsView: View {
+    var body: some View {
+        VStack(spacing: 50) {
+            VStack(spacing: 20.0) {
+                ForEach(appSettings) { setting in
+                    SettingsRowButtonView(text: setting.text, isHavingDestination: setting.isHavingDestination)
+                }
+            }
+            
+            VStack(spacing: 20.0) {
+                ForEach(typicalSettings) { setting in
+                    SettingsRowButtonView(text: setting.text, isHavingDestination: setting.isHavingDestination)
+                }
+            }
+            
+            VStack(spacing: 20.0) {
+                ForEach(actionSettings) { setting in
+                    SettingsRowButtonView(text: setting.text, isHavingDestination: setting.isHavingDestination)
+                }
+            }
+            
+            
+        }.padding(.horizontal)
     }
 }
